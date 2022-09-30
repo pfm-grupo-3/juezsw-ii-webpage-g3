ce7963f17bc30783f95a71ba6c88d78cc430d04d
Ochoa, Manuel (Contractor) <mochoa2@trueblue.com>
2022-09-30 12:50:32 -0500

    fix(dependencies) generating new package lock

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          WEB APP - JUEZSW
-        </p>
-        <p>
-          TFM - GRUPO 3
-        </p>
+        <p>WEB APP - JUEZSW</p>
+        <p>TFM - GRUPO 3</p>
       </header>
     </div>
   );
